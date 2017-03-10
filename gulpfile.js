ee82fc95ccb83a859e65051bc82ac6a5b9ff7f7f
Peter Borkuti <peter@aspire>
2017-03-10 20:42:20 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var metal = require('gulp-metal');
+
+metal.registerTasks({
+	bundleCssFileName: 'playground.css',
+	bundleFileName: 'playground.js',
+	moduleName: 'metal-playground'
+});
